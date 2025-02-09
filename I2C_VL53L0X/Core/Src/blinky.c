fa5cb0176fb1c155e48e2a4a693bb96ace986c98
Khoa A Nguyen <anhkkhoanguyen@gmail.com>
2025-02-09 12:01:47 -0800

    Adding I2C module, finish testing with blinky for cortex debug

---
+++ I2C_VL53L0X/Core/Src/blinky.c
@@ -0,0 +1,10 @@
+#include "stm32f4xx.h"
+
+void BlinkyPeripheral(void) 
+{
+    //Peripheral Clock Enable Register AHB1ENR
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; 
+
+    //GPIO Mode Register (No need to clear for now, but should be in the future for register)
+    GPIOA-> MODER |= (1 << 2); //Enable 01 of MODER1 for output mode
+}
---
+++ I2C_VL53L0X/build/CMakeFiles/I2C_VL53L0X.dir/startup_stm32f411xe.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/I2C_VL53L0X.dir/startup_stm32f411xe.s.obj: \
+ /home/khoa-a-nguyen/Documents/ENGR_Projects/bare_metal_driver/I2C_VL53L0X/startup_stm32f411xe.s
